9c87c8f7410240e9b5a4079f601b238a71c51a56
nicechylegitproperties-dot <nicechylegitproperties@gmail.com>
2026-04-25 02:33:01 +0100

    Fix CORS for Netlify frontend

--- server.js
+++ server.js
@@ -23,7 +23,10 @@ const io = new Server(server, {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+	origin: ['https://localhost:5500', 'https://https://guileless-gingersnap-d19cf5.netlify.app'],
+	credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
